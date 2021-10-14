2eb757cf1b2e694547f50c641c2c4c8c00575c32
Rakasiwi Surya <rakasiwi.surya@gmail.com>
2021-10-15 00:18:22 +0700

    refactor soal 3

--- 3/script.js
+++ 3/script.js
@@ -1,18 +1,22 @@
 function squarePattern(input) {
   let string = "";
-  for (let i = 0; i < input; i++) {
-    let mid = Math.floor(input / 2);
 
-    for (let j = 0; j < input; j++) {
+  for (let i = 1; i <= input; i++) {
+    // * (column)
+    let mid = Math.floor((input + 1) / 2); // * (column)
+
+    for (let j = 1; j <= input; j++) {
+      // * (row)
       if (
-        (j === 0 && i === 0) ||
-        (j === input - 1 && i === 0) ||
-        (j === 0 && i === input - 1) ||
-        (j === input - 1 && i === input - 1) ||
-        j === mid ||
-        i === mid
+        (j === 1 && i === 1) || // * kiri(j) atas(i)
+        (j === input && i === 1) || // * kanan(j) atas(i)
+        (j === 1 && i === input) || // * kiri(j) bawah(i)
+        (j === input && i === input) || // * kanan(j) bawah(i)
+        j === mid || // * vertical
+        i === mid // * horizontal
       ) {
         if (j === mid && i === mid) {
+          // * center point
           string += "#";
         } else {
           string += "*";
